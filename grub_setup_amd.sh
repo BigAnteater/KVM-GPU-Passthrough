78c707532552eca5f59a6f471b653bba9e2a37fc
BigAnteater <77298458+BigAnteater@users.noreply.github.com>
2021-12-04 12:33:19 -0800

    Add files via upload

---
+++ grub_setup_amd.sh
@@ -0,0 +1,26 @@
+if [ $EUID -ne 0 ]
+        then
+                echo "This program must run as root to function." 
+                exit 1
+fi
+
+echo "This script will configure your grub config for virtualization."
+
+GRUB=`cat /etc/default/grub | grep "GRUB_CMDLINE_LINUX_DEFAULT" | rev | cut -c 2- | rev`
+#adds amd_iommu=on and iommu=pt to the grub config
+GRUB+=" amd_iommu=on iommu=pt video=efif:off\""
+sed -i -e "s|^GRUB_CMDLINE_LINUX_DEFAULT.*|${GRUB}|" /etc/default/grub
+
+grub-mkconfig -o /boot/grub/grub.cfg     
+sleep 5s
+              
+echo
+echo "Grub bootloader has been modified successfully, reboot time!"
+echo "press Y to reboot now and n to reboot later."
+read REBOOT
+
+if [ $REBOOT = "y" ]
+        then
+                reboot
+fi
+exit
