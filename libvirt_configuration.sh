dac59920d8850058e8ce548a44cdd6aa7bf2fa4a
BigAnteater <77298458+BigAnteater@users.noreply.github.com>
2021-12-04 07:51:44 -0800

    Add files via upload

---
+++ libvirt_configuration.sh
@@ -0,0 +1,44 @@
+if [ $EUID -ne 0 ]
+	then
+		echo "This program must run as root to function." 
+		exit 1
+fi
+echo "This will install and configure libvirt."
+sleep 1s
+pacman -S libvirt libvirt-glib libvirt-python virt-install virt-manager qemu qemu-arch-extra ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat iptables swtpm
+sleep 1s
+clear
+echo "Now it's time to edit your configs!"
+mv /etc/libvirt/libvirtd.conf /etc/libvirt/libvirtd.conf.old
+echo "mv /etc/libvirt/libvirtd.conf /etc/libvirt/libvirtd.conf.old"
+sleep 1s
+mv libvirtd.conf /etc/libvirt
+echo "mv libvirtd.conf /etc/libvirt"
+sleep 1s
+systemctl enable libvirtd
+echo "systemctl enable libvirtd"
+sleep 1s
+systemctl start libvirtd
+echo "systemctl start libvirtd"
+sleep 2s
+clear
+echo "libvirt has been successfully configured!"
+sleep 2s
+clear
+echo "Time for your QEMU configs babe!"
+sleep 2s
+echo
+echo "Yes, honey"
+sleep 3s
+clear
+echo "mv /etc/libvirt/qemu.conf /etc/libvirt/qemu.conf.old"
+mv /etc/libvirt/qemu.conf /etc/libvirt/qemu.conf.old
+sleep 1s
+echo "mv qemu.conf /etc/libvirt"
+mv qemu.conf /etc/libvirt
+sleep 1s
+clear
+systemctl restart libvirtd
+echo "QEMU has been successfully configured!"
+sleep 5s
+done
