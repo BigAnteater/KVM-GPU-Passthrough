86380048775fffbe682835be722d7b5372ec3313
BigAnteater <77298458+BigAnteater@users.noreply.github.com>
2022-02-25 14:15:01 -0800

    Update grub_setup_amd.sh

--- grub_setup_amd.sh
+++ grub_setup_amd.sh
@@ -8,7 +8,7 @@ echo "This script will configure your grub config for virtualization."
 
 GRUB=`cat /etc/default/grub | grep "GRUB_CMDLINE_LINUX_DEFAULT" | rev | cut -c 2- | rev`
 #adds amd_iommu=on and iommu=pt to the grub config
-GRUB+=" amd_iommu=on iommu=pt video=efif:off\""
+GRUB+=" amd_iommu=on iommu=pt video=efifb:off\""
 sed -i -e "s|^GRUB_CMDLINE_LINUX_DEFAULT.*|${GRUB}|" /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg     
