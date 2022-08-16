184de3bf0117c3657a766191829056de53433d02
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-08-15 20:16:43 -0400

    Update libvirt_configuration_redhat.sh

--- libvirt_configuration_redhat.sh
+++ libvirt_configuration_redhat.sh
@@ -7,7 +7,7 @@ if [ $EUID -ne 0 ]
 fi
 echo "This will install and configure libvirt."
 sleep 1s
-dnf install libvirt libvirt-glib libvirt-python virt-install virt-manager qemu qemu-common ebtables dnsmasq bridge-utils iptables swtpm
+dnf install libvirt libvirt-glib virt-install virt-manager qemu qemu-common ebtables dnsmasq bridge-utils iptables swtpm
 sleep 1s
 systemctl enable libvirtd
 echo "systemctl enable libvirtd"
